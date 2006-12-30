b1343eae0f6df8d6449ed7a8c5f51ef79a47b961
caleb <caleb@912e46cf-8c48-47e7-bf05-d0122df541e3>
2006-12-30 20:43:07 +0000

    Modified the FooBar.java class so that it extends FooBarParent. This class is in another directory and is not in the src. This allows me to test the 'classpath' attribute.
    Added a workaround to the setup() method in test.rb to accomodate the JRuby ENV[] bug.

---
+++ test-resources/parent-src/parent/FooBarParent.java
@@ -0,0 +1,11 @@
+package parent;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: caleb
+ * Date: Dec 30, 2006
+ * Time: 3:08:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class FooBarParent {
+}
