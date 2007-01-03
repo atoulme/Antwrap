345e56afcd281c48f49fdcafd521f94c8f35e852
caleb <caleb@912e46cf-8c48-47e7-bf05-d0122df541e3>
2007-01-03 04:41:56 +0000

    Made some improvements. Can now convert a build.xml file's target's and tasks to a rakefile. Still need to to deal with properties and other tricky things.

--- lib/convert.rb
+++ lib/convert.rb
@@ -1,20 +1,54 @@
 require 'rexml/document'
 
-xml = REXML::Document.new(File.open('/Users/caleb/projects/antwrap/test/test-resources/build.xml'))
+def create_symbol(str)
+  if (str == nil) 
+    str = ''
+  else
+    str = str.gsub(/(\w*)[\-|\.](\w*)/, '\1_\2')
+    str = str.gsub(/(\w*[^,\s])/, ':\1')
+  end
+  return str
+end
 
-xml.elements.each("//target") do |target| 
-  name = target.attributes['name']
-  depends = target.attributes['depends']
-  foo = "name[#{name}]"
-  if depends
-    foo = foo + " depends[#{depends}]"
+@one_tab= '   '
+@two_tab= '   '
+def print_child(parent_task, child)
+  puts "#{@one_tab}#{parent_task}.add(#{child.name}("
+  child.attributes.each do |key, value|
+    puts "#{@one_tab}:#{key} => \"#{value}\","
   end
-  puts '***************************'
-  puts foo
-  target.elements.each do |element|
-    puts "child: #{element}"
-  end
-  puts '***************************'
+  puts "#{@one_tab})\n"
   
+end
+def print_task(task)
+  puts "#{@one_tab}#{task.name}_task = #{task.name}("
+  
+  task.attributes.each do |key, value|
+    puts "#{@one_tab}:#{key} => \"#{value}\",\n"
+  end
+  puts "#{@one_tab})"
+  
+  task.elements.each do |child|
+    print_child("#{task.name}_task", child)
+  end
+  
+  puts "#{@one_tab}#{task.name}_task.execute() \n"
+  
+end
+
+xml = REXML::Document.new(File.open('/Users/caleb/projects/antwrap/test/test-resources/build.xml'))
+xml.elements.each("//target") do |target| 
+  
+  task = "task " + create_symbol(target.attributes['name']) + 
+         " => [" + create_symbol(target.attributes['depends']) + "] do"
+  
+  puts task
+  
+  target.elements.each do |element|
+    print_task(element)
+  end
+  
+  puts 'end'
+  puts ''
   
 end
